b778e9fccb304dfb5e03c300ab95c6d782b97481
kworster <kworster@users.noreply.github.com>
2025-02-23 20:47:28 +0000

    deploy: f41c29c24881a7d9615bdd55eb9eb020be92f009

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { doc, getDocs, addDoc, updateDoc, getFirestore, collection } from "firebase/firestore";
-import log from "loglevel";
 
 const sw = new URL('service-worker.js', import.meta.url)
 if ('serviceWorker' in navigator) {
@@ -14,131 +13,101 @@ if ('serviceWorker' in navigator) {
 
 const firebaseConfig = {
     apiKey: "AIzaSyAbLefRO7pNCWYvmLtP7jaYZgJVTpURCRk",
-    authDomain: "scheduler-13980.firebaseapp.com",
-    projectId: "scheduler-13980",
-    storageBucket: "scheduler-13980.firebasestorage.app",
-    messagingSenderId: "853608682087",
-    appId: "1:853608682087:web:9d0c4898b2f0f9040cbac2",
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
+  authDomain: "scheduler-13980.firebaseapp.com",
+  projectId: "scheduler-13980",
+  storageBucket: "scheduler-13980.firebasestorage.app",
+  messagingSenderId: "853608682087",
+  appId: "1:853608682087:web:9d0c4898b2f0f9040cbac2",
+};
+  
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
 
-// Add Task
-// addTaskBtn.addEventListener('click', () => {
-//     const task = taskInput.value.trim();
-//     if (task) {
-//         const li = document.createElement('li');
-//         li.textContent = task;
-//         taskList.appendChild(li);
-//         taskInput.value = '';
-//     }
-// });
+window.addEventListener('load', () => {
+  renderTasks();
+});
 
+// Add Task
 addTaskBtn.addEventListener('click', async () => {
     const task = taskInput.value.trim();
     if (task) {
-        const taskInput = document.getElementById("taskInput");
-        const taskText = sanitizeInput(taskInput.value.trim());
-
-        if (taskText) {
-            await addTaskToFirestore(taskText);
-            renderTasks();
-            taskInput.value = "";
-        }
-        renderTasks();
+        let taskId = await addTaskToFirestore(task);
+        taskInput.value = "";
+        
+        createLiTask(taskId, task);
+    } else {
+        alert("Please enter a task!");
     }
 });
 
-// Remove Task on Click
-// taskList.addEventListener('click', (e) => {
-//     if (e.target.tagName === 'LI') {
-//         e.target.remove();
-//     }
-// });
-
+// Remove Task
 taskList.addEventListener('click', async (e) => {
-    if (e.target.tagName === 'LI') {
-      await updateDoc(doc(db, "scheduler", e.target.id), {
-        completed: true
-      });  
-    }
-    renderTasks();
-  });
+  if (e.target.tagName === 'LI') {
+    await updateDoc(doc(db, "scheduler", e.target.id), {
+      completed: true
+    });  
+    e.target.remove();
+  }
+});
 
-  //Render Task
-  async function renderTasks() {
+
+async function renderTasks() {
     var tasks = await getTasksFromFirestore();
     taskList.innerHTML = "";
-  
-    tasks.forEach((task, index) => {
-      if(!task.data().completed){
-        const taskItem = document.createElement("li");
-        taskItem.id = task.id;
-        taskItem.textContent = task.data().text;
-        taskList.appendChild(taskItem);
+
+    let taskArr = [];
+
+    tasks.forEach(task => {
+      taskArr.push({
+        "id" : task.id,
+        "text": task.data().text,
+        "completed": task.data().completed
+      })
+    });
+
+    taskArr.sort(function(a,b){
+      return new Date(b.timeCreated) - new Date(a.timeCreated);
+    });
+
+    taskArr.forEach(task => {
+      if(!task.completed){
+        createLiTask(task.id, task.text);
       }
     });
   }
 
   async function addTaskToFirestore(taskText) {
-    await addDoc(collection(db, "scheduler"), {
+    let task = await addDoc(collection(db, "checklist"), {
       text: taskText, 
       completed: false
     });  
+    return task.id;
   }
 
   async function getTasksFromFirestore() {
-    var data = await getDocs(collection(db, "scheduler"));
-    let userData = [];
-    data.forEach((doc) => {
-      userData.push(doc);
-  });
-  return userData;
-}
-
-  function sanitizeInput(input) {
-    const div = document.createElement("div");
-    div.textContent = input;
-    return div.innerHTML;
+    return await getDocs(collection(db, "checklist"));
   }
 
-//Error Logging
-window.addEventListener('error', function (event) {
-    console.error('Error occurred: ', event.message);
-});
+  function createLiTask(id, text) {
+    let taskItem = document.createElement("li");
+    taskItem.id = id;
+    taskItem.textContent = text;
+    taskItem.tabIndex = 0;
+    taskList.appendChild(taskItem);
+  }
 
-// Set the log level (trace, debug, info, warn, error)
-log.setLevel("info");
-
-// Example logs
-log.info("Application started");
-log.debug("Debugging information");
-log.error("An error occurred");
-
-function addTask(task) {
-    try {
-        // Log user action
-        log.info(`Task added: ${task}`);
-        // Add task to the list
-        tasks.push(task);
-        renderTasks();
-    } catch (error) {
-        // Log error
-        log.error("Error adding task", error);
-    }
-}
-
-taskInput.addEventListener("keypress", function(event) {
+  //Allow task addition on enter key while in task input
+  taskInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
       addTaskBtn.click();
     }
   });
 
+  //Allow tasks to be completed on enter
   taskList.addEventListener("keypress", async function(e) {
     if (e.target.tagName === 'LI' && e.key === "Enter") {
       await updateDoc(doc(db, "scheduler", e.target.id), {
@@ -147,3 +116,7 @@ taskInput.addEventListener("keypress", function(event) {
     }
     renderTasks();
   });
+
+window.addEventListener('error', function (event) {
+    console.error('Error occurred: ', event.message);
+});
