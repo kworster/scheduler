372a41ad2143eba15de34a83d11c24f63d6766a2
kworster <krystleworster@gmail.com>
2025-02-28 00:40:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -118,11 +118,7 @@ async function renderTasks() {
     renderTasks();
   });
 
-  const querySnapshot = await getDocs(collection(db, "scheduler"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-});
-
+ 
 window.addEventListener('error', function (event) {
     console.error('Error occurred: ', event.message);
 });
